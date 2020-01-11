421020910799ab0ba540dcac517ec920cce560af
Egor Yakimovich <yakimovich.egor@gmail.com>
2020-01-11 15:55:11 +0300

    feat: add php form

---
+++ send_control.php
@@ -0,0 +1,38 @@
+<?php
+
+
+$control_userName = $_POST['control_userName'];
+$control_userPhone = $_POST['control_userPhone'];
+
+// Load Composer's autoloader
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'egyakim@gmail.com';                     // SMTP username
+    $mail->Password   = 'Egyak_2020';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('egyakim@gmail.com');
+    $mail->addAddress('songlk@yandex.by');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${control_userName}, Телефон пользователя: ${control_userPhone}";
+    $mail->send();
+    header('Location: thanks.html');
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
